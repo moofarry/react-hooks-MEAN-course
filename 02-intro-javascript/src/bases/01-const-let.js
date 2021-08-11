e939d615f699e3c3ae09f1683a98a6b513e2d142
moofarry <jhon.moofarry00@usc.edu.co>
2021-08-11 18:38:51 -0500

    bases-const-let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+const name = 'Jhon';
+const lastName = 'Moofarrif';
+
+let valueDices = 4;
+valueDices = 5;
+
+console.log("zzzs", name, lastName, valueDices)
+
+if (true) {
+  const name = 'Peter' 
+  console.log(`name`, name)
+}
+console.log(`valueDices out if`, valueDices)
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,6 +1,1 @@
-const nombre = 'Jhon';
-const apellido = 'Moofarrif';
-
-let valorDado = 4;
-
-console.log( "gggggg",nombre,apellido,valorDado)
+console.log(`??`)
