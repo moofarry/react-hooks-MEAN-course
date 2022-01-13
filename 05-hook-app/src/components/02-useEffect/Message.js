b529e4175f826cfa35dc2fd823766c138d1987ca
Jhon Moofarry <jhon.moofarry00@gmail.com>
2022-01-13 08:44:40 -0500

    useEffect with useForm

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    window.addEventListener("mousemove", (e) => {
-      const coors = { x: e.x, y: e.y };
-      console.log(coors);
-    });
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
 
     return () => {
-      console.log("com  ponent down ");
+      window.removeEventListener(" ", mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h2> You are cool!</h2>
+      <p>
+        x: {x} y:{y}
+      </p>
     </div>
   );
 };
