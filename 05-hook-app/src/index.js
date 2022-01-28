7a17fdd6532f6f8801343ce161488fa088938395
Jhon Moofarry <jhon.moofarry00@gmail.com>
2022-01-28 08:53:04 -0500

    useLayoutEffect with useRef

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
-import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+// import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+import { LayoutEffect } from "./components/05-useLayaoutEffect/LayoutEffect";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
 //import { CounterApp } from "./components/01-useState/CounterApp";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<RealExampleRef />, document.getElementById("root"));
+ReactDOM.render(<LayoutEffect />, document.getElementById("root"));
