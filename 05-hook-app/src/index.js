8be8cde819d959b484cbd5b2220fd3dab80f09ec
Jhon Moofarry <jhon.moofarry00@gmail.com>
2022-01-31 09:01:38 -0500

    React.memo

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Memo } from "./components/06-memo/Memo";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
-import { LayoutEffect } from "./components/05-useLayaoutEffect/LayoutEffect";
+// import { LayoutEffect } from "./components/05-useLayaoutEffect/LayoutEffect";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 // import CounterWithCustomHook from "./components/01-useState/CounterWithCustomHook";
 //import { CounterApp } from "./components/01-useState/CounterApp";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<LayoutEffect />, document.getElementById("root"));
+ReactDOM.render(<Memo />, document.getElementById("root"));
