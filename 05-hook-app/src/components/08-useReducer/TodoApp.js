970dcc84745cddf30fb3d07be3f33275803bb707
Jhon Moofarry <jhon.moofarry00@gmail.com>
2022-04-15 00:35:08 -0500

    performance TODO 2

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -3,6 +3,7 @@ import { todoReducer } from "./todoReducer";
 import "./styles.css";
 import { useForm } from "../../hooks/useForm";
 import { TodoList } from "./TodoList";
+import { TodooAdd } from "./TodooAdd";
 
 const init = () => {
   return JSON.parse(localStorage.getItem("todos")) || [];
@@ -10,32 +11,10 @@ const init = () => {
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: "",
-  });
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim() <= 1) {
-      return;
-    }
-    const newTodo = {
-      id: new Date().getTime(),
-      description: description,
-      done: false,
-    };
-    const action = {
-      type: "add",
-      payload: newTodo,
-    };
-    dispatch(action);
-    reset();
-  };
 
   const handleRemove = (todoId) => {
     const action = {
@@ -52,9 +31,13 @@ export const TodoApp = () => {
     });
   };
 
+  const handleAddTodo = (newTodo) => {
+    dispatch({ type: "add", payload: newTodo });
+  };
+
   return (
     <div>
-      <h1>Cosas por hacer morchis : ({todos.length})</h1>
+      <h1>TodoApp: ({todos.length})</h1>
       <hr />
       <div className="row">
         <div className="col-6 me-5">
@@ -65,25 +48,7 @@ export const TodoApp = () => {
           />
         </div>
         <div className="col-4">
-          <h4>Agregar TODO</h4>
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              className="form-control"
-              type="text"
-              name="description"
-              placeholder="Add new TODO"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={description}
-            />
-            <button
-              className="btn btn-outline-primary mt-1 w-100 "
-              type="submit"
-            >
-              Agregar
-            </button>
-          </form>
+          <TodooAdd handleAddTodo={handleAddTodo} />
         </div>
       </div>
     </div>
---
+++ 05-hook-app/src/components/08-useReducer/TodooAdd.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useForm } from "../../hooks/useForm";
+
+export const TodooAdd = ({ handleAddTodo }) => {
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (description.trim() <= 1) {
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      description: description,
+      done: false,
+    };
+    handleAddTodo(newTodo);
+    reset();
+  };
+  return (
+    <>
+      <h4>Agregar TODO</h4>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control"
+          type="text"
+          name="description"
+          placeholder="Add new TODO"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={description}
+        />
+        <button className="btn btn-outline-primary mt-1 w-100 " type="submit">
+          Agregar
+        </button>
+      </form>
+    </>
+  );
+};
